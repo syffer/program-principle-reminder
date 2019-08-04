65450e9411bedfdbf541935db493a58b4f06a172
syffer <maxime.nicolas2@hotmail.fr>
2019-08-04 18:56:29 +0200

    passage à androidx et mise à jour des librairies (room, lifecycle, navigation)

--- app/src/main/java/com/syffer/pincreminder/MainActivity.kt
+++ app/src/main/java/com/syffer/pincreminder/MainActivity.kt
@@ -1,10 +1,10 @@
 package com.syffer.pincreminder
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -13,12 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+        //setSupportActionBar(toolbar)
 
+        /*
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+        */
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
